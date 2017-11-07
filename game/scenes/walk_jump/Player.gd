c16740106234cd3f8aed44b806a2775bf9c06855
Paflo <pietro1991a@gmail.com>
2017-11-07 22:43:00 +0100

    Improve walk

--- game/scenes/walk_jump/Player.gd
+++ game/scenes/walk_jump/Player.gd
@@ -8,8 +8,7 @@ extends KinematicBody2D
 var input_direction = 0  # può essere 0, 1 (destra) o -1 (sinistra)
 var direction = 0
 
-var speed_x = 0  # es: 200 pixel per secondo (indipendente dalla direzione)
-var speed_y = 0
+var speed = Vector2()  # es: 200 pixel per secondo (indipendente dalla direzione)
 var velocity = Vector2()  # velocity è un VETTORE, dipende dalla velocità, dalla direzione e dal delta (tempo)
 
 const MAX_SPEED = 600
@@ -30,7 +29,7 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("jump"):
-		speed_y = - JUMP_FORCE
+		speed.y = - JUMP_FORCE
 
 
 func _process(delta):
@@ -51,30 +50,47 @@ func _process(delta):
 
 	# MOVEMENT
 	if input_direction == -direction:
-		speed_x /= 3  # in modo da diminuire velocità se cambiamo direzione
+		speed.x /= 3  # in modo da diminuire velocità se cambiamo direzione
 	if input_direction:
-		speed_x += ACCELERATION * delta
+		speed.x += ACCELERATION * delta
 	else:
-		speed_x -= DECELERATION * delta
+		speed.x -= DECELERATION * delta
 
 	# Abbiamo sommato alla velocità attuale l'accelerazione per l'intervallo di
 	# tempo (di frame, delta) in modo da accelerare solo per quegli istanti di
 	# tempo, ottenendo un'animazione più fluida e veritiera. Ora come ora però la
 	# nostra speed può aumentare a dismisura!!!! dobbiamo stabilire un range:
-	speed_x = clamp(speed_x, 0, MAX_SPEED)
+	speed.x = clamp(speed.x, 0, MAX_SPEED)
 
-	speed_y += GRAVITY * delta  # in modo da dare un'accelerazione al nostro pg anche lungo l'asse y
+	speed.y += GRAVITY * delta  # in modo da dare un'accelerazione al nostro pg anche lungo l'asse y
 
 	# direction anziché input direction (non vogliamo che sia 0) ora abbiamo
 	# tutto, dobbiamo solo far muovere il pg lungo il nostro vettore velocità
 	# (che ovviamente ha x = velocity;  y = 0 ) ( la componente y dipenderà dal
 	# salto )
-	velocity.x = speed_x * delta * direction
-	velocity.y = speed_y * delta
+	velocity = Vector2(speed.x * delta * direction, speed.y * delta)
 
-	move(velocity)
-
-	# dobbiamo poi aggiungere un collision e poi il ground o il nostro pg 
-	# continua a cadere infinitamente; nei platform è altamente raccomandato 
+	# dobbiamo poi aggiungere un collision e poi il ground o il nostro pg
+	# continua a cadere infinitamente; nei platform è altamente raccomandato
 	# utilizzare collision rettangolari o quadrati, il nostro, shape è quadrato
 	# altro consiglio utile, l'hitbox farla leggermente più piccola del pg
+
+	# Problema, il pg, a meno di salti, rimane "ancorato" al ground, questo perché
+	# dopo una collisione la posizione è ripristinata (di nascosto all'occhio umano)
+	# tangente all'oggetto "colleso"; dobbiamo permettere al nostro eroe di
+	# camminare, a tal scopo è utile ricordare il vettore velocità precedente alla
+	# collisione:
+	var movement_reminder = move(velocity)
+
+	if is_colliding():
+		# calcoliamo il vettore normale della collisione e ricordandoci della nostra
+		# ultima direzione con slide costruiamo un movimento che escluda la collisione
+		var normal = get_collision_normal()
+		var final_movement = normal.slide(movement_reminder)
+		# ovviamente non possiamo ignorarla totalmente, essendoci stata una collisione
+		# dobbiamo resettare la speed lungo l'asse y che altrimenti si accumula
+		# durante tutto il tempo della collisione col ground, possiamo scrivere
+		# speed.y = 0; ma per collisioni con piani inclinati, e non perfettamente
+		# orizzontali non è fisicamente coerente, meglio:
+		speed = normal.slide(speed)
+		move(final_movement)
