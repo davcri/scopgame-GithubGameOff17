1a6dc5ee9a16f9d0a017458539d9bbd12aba3848
Paflo <pietro1991a@gmail.com>
2017-11-08 00:39:43 +0100

    Limits the jumps to only one, available again each time you touch the ground and improve walk

--- game/scenes/walk_jump/Player.gd
+++ game/scenes/walk_jump/Player.gd
@@ -1,35 +1,50 @@
 extends KinematicBody2D
-
+# ci serve per accedere allo sprite e fargli fare il flip (es girare a dx se
+# si sta muovendo a destra) per accedervi dobbiamo essere sicuri che lo sprite
+# sia stato già creato, "inizializzato" da godot, ecco perché lo
+# utilizziamo nella func _ready
+var sprite_node
 # NB: se non introduciamo "last direction"(input_direction) non appena il
 # giocatore rilascia left o right, il pg si ferma istantaneamente (perché alla
 # fine del codice moltiplichiamo per la direzione (che in questo caso è
 # nulla) , per risolvere distinguiamo tra "input_direction" e "direction" per
 # dare un'animazione più "smooth" fluida xD
 var input_direction = 0  # può essere 0, 1 (destra) o -1 (sinistra)
-var direction = 0
+var direction = 1
 
 var speed = Vector2()  # es: 200 pixel per secondo (indipendente dalla direzione)
 var velocity = Vector2()  # velocity è un VETTORE, dipende dalla velocità, dalla direzione e dal delta (tempo)
 
-const MAX_SPEED = 600
-const ACCELERATION = 1000  # 1000 pixel per secondo al secondo
-const DECELERATION = 2000
+const MAX_SPEED = 442
+const ACCELERATION = 2600  # 2600 pixel per secondo al secondo
+const DECELERATION = 5000
 
 # jf "quanto in alto salta" forza opposta alla forza di gravità; nella maggior
 # parte degli engine l'asse y è negativo in alto e positivo in basso
 # orientato in modo da assecondare la forza di gravità. Ecco perché in
 # func input è MENO JUMP_FORCE (l'asse x rimane invariato)
-const JUMP_FORCE = 800
+const JUMP_FORCE = 900
 const GRAVITY = 2000
+const MAX_FALL_SPEED = 1400
+
+# limiteremo a 1 il numero di salti che può compiere il giocatore prima di
+# ritoccare terra (già 2 sarebbero esagerati nel nostro caso)
+var jump_count = 0
+# rendendola una var anziché const si potrebbe, più avanti nel gioco, con
+# qualche upgrade potenziarla, in questo caso non ci interessa
+const MAX_JUMP_COUNT = 1
 
 func _ready():
 	set_process(true)
 	set_process_input(true)
+	# va inserito il path, essendo child diretto basta "Sprite"
+	sprite_node = get_node("Sprite")
 
 
 func _input(event):
-	if event.is_action_pressed("jump"):
+	if jump_count < MAX_JUMP_COUNT and event.is_action_pressed("jump"):
 		speed.y = - JUMP_FORCE
+		jump_count += 1
 
 
 func _process(delta):
@@ -43,8 +58,10 @@ func _process(delta):
 
 	if Input.is_action_pressed("move_left"):
 		input_direction = -1
+		sprite_node.set_flip_h(true) # true perché di default guarda a destra
 	elif Input.is_action_pressed("move_right"):
 		input_direction = 1
+		sprite_node.set_flip_h(false)
 	else:
 		input_direction = 0
 
@@ -63,6 +80,8 @@ func _process(delta):
 	speed.x = clamp(speed.x, 0, MAX_SPEED)
 
 	speed.y += GRAVITY * delta  # in modo da dare un'accelerazione al nostro pg anche lungo l'asse y
+	if speed.y > MAX_FALL_SPEED:
+		speed.y = MAX_FALL_SPEED  # abbiamo limitato la velocità in caduta
 
 	# direction anziché input direction (non vogliamo che sia 0) ora abbiamo
 	# tutto, dobbiamo solo far muovere il pg lungo il nostro vettore velocità
@@ -94,3 +113,8 @@ func _process(delta):
 		# orizzontali non è fisicamente coerente, meglio:
 		speed = normal.slide(speed)
 		move(final_movement)
+		# dobbiamo ripristinare a 0 il jump_count ogni volta che il pg tocca terra
+		# se non mettessimo la clausola if, saltando sui muri salterebbe all'infinito
+		# se il vettore normale // asse y (0, +-1) allora si tratta del ground
+		if normal == Vector2(0 , -1):
+			jump_count = 0
