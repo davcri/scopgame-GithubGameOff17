4247d9beabc850ef686acc5178a6ece42c546655
Unknown <pasquale.salvati91@gmail.com>
2017-11-14 21:53:06 +0100

    Prova commit refactoring dialog box

---
+++ game/scenes/common/DialogBox/TextLabel.gd
@@ -0,0 +1,36 @@
+#TextLabel
+extends RichTextLabel
+
+# class member variables go here, for example:
+
+var dialog = ["Steam's saves are coming", "you must buy"]
+var page = 0 
+var timer
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	set_bbcode(dialog[page])
+	set_visible_characters(0)
+
+func _on_Timer_timeout():
+	
+	set_visible_characters(get_visible_characters()+1)
+	if get_visible_characters() > get_total_character_count():
+		if page < dialog.size() -1:
+			page +=1
+			set_bbcode(dialog[page])
+			set_visible_characters(0)
+	timer.start()
+
+
+func _on_DreamLordTrigger_body_enter( body ):
+	if body.get_name() == "Player":
+		timer = get_node("Timer")
+		timer.set_wait_time(.05)
+		timer.start()
+	if get_visible_characters() > get_total_character_count():
+		if page < dialog.size() -1:
+			page +=1
+			set_bbcode(dialog[page])
+			set_visible_characters(0)
