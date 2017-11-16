3f25794033c2d5e5beb64f9c475256514c83976b
Davide Cristini <davcri91@gmail.com>
2017-11-16 10:02:00 +0100

    Add selector movement and animations for smartphone lock puzzle

--- Lock.gd
+++ Lock.gd
@@ -3,16 +3,48 @@ extends GridContainer
 const NODE_PER_ROW = 3
 const NODE_PER_COLUMN = 3
 
+# 1 2 3
+# 4 5 6
+# 7 8 9
 var buttons = []
 
-#   1 2 3
-#   4 5 6
-#   7 8 9
-var selector = Vector2(0, 0)
+var selector = Vector2()
 
+onready var tween = get_node("Tween")
 
 func _ready():
+	_initialize_buttons()
+	# set_fixed_process(true)
+	update_selector_pos(1, 1)
+	
+# func _fixed_process(delta):
+	
+
+func get_selector_texture_button():
+	return buttons[selector.y][selector.x]
+
+
+func update_selector_pos(x, y):
+	deactivate(selector.x, selector.y)
+	tween.remove_all()
+	
+	# Updates selector coordinates
+	selector.x = x
+	selector.y = y
+	activate(selector.x, selector.y)
+	
+	# Activate tween animation on the selector
+	var duration = 0.5
+	tween.interpolate_method(get_selector_texture_button(), "set_opacity", 1, 0, duration, Tween.TRANS_SINE, Tween.EASE_IN)
+	tween.interpolate_method(get_selector_texture_button(), "set_opacity", 0, 1, duration, Tween.TRANS_SINE, Tween.EASE_OUT, duration)
+	tween.set_repeat(true)
+	tween.start()
+
+
+func _initialize_buttons():
 	get_node("TextureButton").set_toggle_mode(true)
+	get_node("TextureButton").set_opacity(0.5)
+	get_node("TextureButton").set_ignore_mouse(true)  # Ignores mouse clicks
 	
 	for i in range(NODE_PER_ROW):
 		var row = []
@@ -20,18 +52,18 @@ func _ready():
 		for j in range(NODE_PER_COLUMN):
 			if i == 0 && j == 0:
 				row.append(get_node("TextureButton"))
-				
 			else:
-				var b = get_node("TextureButton").duplicate(true)  # ???
+				var b = get_node("TextureButton").duplicate()
 				row.append(b)
 				add_child(b)
 		
-		print(row)
 		buttons.append(row)
 
-	get_node("TextureButton").set_pressed(true)
-	activate(selector.x, selector.y)
+	# get_node("TextureButton").set_pressed(true)
 
 
 func activate(i, j):
-	buttons[i][j].set_pressed(true)
+	buttons[j][i].set_pressed(true)
+
+func deactivate(i, j):
+	buttons[j][i].set_pressed(false)
--- game/scenes/common/Puzzle/Smartphone.gd
+++ game/scenes/common/Puzzle/Smartphone.gd
@@ -3,14 +3,33 @@ extends CanvasLayer
 # class member variables go here, for example:
 # var a = 2
 var sequence = []
+onready var lock = get_node("Smartphone/LockNodes")
 
 func _ready():
 	print("ASD")
 	set_process_input(true)
 	
 func _input(event):
+	var pos = lock.selector
+	
 	if event.is_action_released("move_left"):
 		print("LEFT")
+		if pos.x > 0:
+			pos.x = pos.x - 1
+			lock.update_selector_pos(pos.x, pos.y)
 	elif event.is_action_released("move_right"):
 		print("RIGHT")
+		if pos.x < lock.NODE_PER_ROW - 1:
+			pos.x = pos.x + 1
+			lock.update_selector_pos(pos.x, pos.y)
+	elif event.is_action_released("move_up"):
+		print("UP")
+		if pos.y > 0:
+			pos.y = pos.y - 1
+			lock.update_selector_pos(pos.x, pos.y)
+	elif event.is_action_released("move_down"):
+		print("DOWN")
+		if pos.y < lock.NODE_PER_COLUMN - 1:
+			pos.y = pos.y + 1
+			lock.update_selector_pos(pos.x, pos.y)
 	
