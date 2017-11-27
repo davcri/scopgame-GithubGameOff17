897848656fce721032d7ff08eec6fdf31d052294
Davide Cristini <davcri91@gmail.com>
2017-11-27 16:21:18 +0100

    Little code clean

--- game/scenes/common/FadeScene/TextureFrame.gd
+++ game/scenes/common/FadeScene/TextureFrame.gd
@@ -7,12 +7,12 @@ func _ready():
 	get_tree().get_root().connect("size_changed", self, "on_screen_resize")
 	
 	# Make sure that the node is in the origin
-	set_pos(Vector2(0,0))
+	set_pos(Vector2(0, 0))
 
 func _draw():
 	# Draws the TextureFrame on top of the viewport area (the entire screen)
 	draw_rect(get_viewport_rect(), color)
 	 
-func on_screen_resize():
+func on_screen_resize():	
 	# Updates the size of the TextureFrame
 	self.set_size(get_viewport_rect().size)
